af62b6ea8960a1e25e721d9027b588442950158a
Jinto Jose <jintoppy@gmail.com>
2022-04-14 08:39:00 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from './user.service';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'http-client';
+  users:User[] = []
+  constructor(private userService: UserService){}
+
+  ngOnInit(){
+    this.userService.getUsers()
+      .subscribe((users: User[]) => {
+        this.users = users;
+      })
+  }
 }
