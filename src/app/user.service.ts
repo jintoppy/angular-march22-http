863d835d9627e6134d3c756a8d07b834bd372fab
Jinto Jose <jintoppy@gmail.com>
2022-04-14 08:44:03 +0200

    update

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -10,6 +10,11 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   getUsers(){
+    //return fetch('https://api.github.com/users');
     return this.http.get<User[]>('https://api.github.com/users');
+  }
+
+  searchUsers(searchTerm:string){
+    //https://api.github.com/search/users?q=angular
   }
 }
